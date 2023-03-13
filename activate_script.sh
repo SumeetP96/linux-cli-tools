e84e65981198e0d04bae9cacdabc7aa7d9c1f2f8
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2023-03-13 13:11:03 +0530

    add replace option for existing scripts

--- activate_script.sh
+++ activate_script.sh
@@ -18,23 +18,42 @@ function get_new_name() {
     read new_name
 }
 
-function activate_script() {
-    FILE="$destination/$new_name"
+function generate_activated_script() {
+    sudo chmod u+x $source
+    sudo ln $source /usr/local/bin/$new_name
 
-    if [ -f "$FILE" ]; then
-        echo $FILE
-        echo "File with the same name already exists. Please choose a different name."
-        get_new_name
-        activate_script
+    local SCRIPT_FILE="$destination/$new_name"
+    echo $SCRIPT_FILE
+    if [ -f $SCRIPT_FILE ]; then
+        echo "Script activated and symlink has been created at $SCRIPT_FILE"
     else
-        sudo chmod u+x $source
-        sudo ln $source /usr/local/bin/$new_name
-        if [ -f $FILE ]; then
-            echo "Script activated and symlink has been created at $FILE"
-        else
-            echo "Failed to activate script!"
-            exit 1
-        fi
+        echo "Failed to activate script!"
+        exit 1
+    fi
+}
+
+function activate_script() {
+    local SCRIPT_FILE="$destination/$new_name"
+    if [ -f "$SCRIPT_FILE" ]; then
+        echo $SCRIPT_FILE
+        echo "Script with the same name already exists."
+
+        echo -n -e "\nDo you want to replace it? [y/N] "
+        read confirm_replace
+
+        case $confirm_replace in
+            [yY] ) echo yes;
+                sudo rm $SCRIPT_FILE;
+                if [ $? -eq 0 ]; then
+                    generate_activated_script; 
+                fi
+                ;;
+            * ) get_new_name;
+                activate_script;
+                ;;
+        esac
+    else
+        generate_activated_script
     fi
 }
 
